aadfa5b298d7a6a55fcffc210f0ba1cf61386148
royarijit <royarijit@users.noreply.github.com>
2020-06-11 12:54:27 +0530

    Updates

---
+++ precache-manifest.bc6890a4147ecf5e22d10a360e6460cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3290379222b3c6f1f9d45e94567ef00e",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "af3651973828ff8329c0",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "169db92b2963a491015e",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3651973828ff8329c0",
+    "url": "/react-deploy/static/js/main.f95bd53a.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  }
+]);
